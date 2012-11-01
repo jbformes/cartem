c89c6f31118fad63c77a3b69bdc158ed36e7b471
Rogerio Alexandre <rogerioaba@gmail.com>
2012-11-01 09:58:26 -0200

    starting app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cartem::Application.config.secret_token = 'd6de717471985b59b4960e54875041a0b8f692f468091d7f7dae174dfafcdb19b462b2e6074329235a6a29892def0f26260ce0d17ae0caeb3dae15be78762959'

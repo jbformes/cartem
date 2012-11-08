f27ceef59dea88f9880395243407894dae0c0253
Rogerio Alexandre <rogerioaba@gmail.com>
2012-11-08 14:53:20 -0200

    installing awsome_nested_set gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :parent_id
+
+  acts_as_nested_set
 
   has_many :companies
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121102142642) do
+ActiveRecord::Schema.define(:version => 20121108164213) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "type"
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
   end
 
   create_table "companies", :force => true do |t|
